168da3897dfabfb3721c941c0fd5bb7f604176c3
Huy-DNA <huydo862003@gmail.com>
2024-09-06 22:17:39 +0700

    fix: add NOT NULL and improve PRIMARY KEYs

--- server/db/migrations/1.05-09-2024.init.sql
+++ server/db/migrations/1.05-09-2024.init.sql
@@ -2,49 +2,49 @@ CREATE TYPE user_role as ENUM ('admin', 'lab_manager', 'user');
 
 CREATE TABLE users (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  admin_labs jsonb, -- format: { id: string; location: Location }[]
+  admin_labs jsonb NOT NULL, -- format: { id: string; location: Location }[]
   name text CHECK ((char_length(name) <= 256)),
-  meta jsonb,
-  borrowed_devices jsonb, -- computed using trigger; format: { id: string; name: string }[]
-  default_role user_role
+  meta jsonb NOT NULL,
+  borrowed_devices jsonb NOT NULL, -- computed using trigger; format: { id: string; name: string }[]
+  default_role user_role NOT NULL
 );
 
 CREATE TABLE device_kinds (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   category_id uuid,
   name text CHECK ((char_length(name) <= 256)),
-  meta jsonb,
-  available_quantity jsonb -- computed using trigger; format: { [ location: string ]: number }
+  meta jsonb NOT NULL,
+  available_quantity jsonb NOT NULL -- computed using trigger; format: { [ location: string ]: number }
 );
 
 CREATE TABLE categories (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name jsonb -- format: { [ faculty: string ]: 'Level 1/Level 2/Level 3' }
+  name jsonb NOT NULL -- format: { [ faculty: string ]: 'Level 1/Level 2/Level 3' }
 );
 
 CREATE TYPE reservation_status as ENUM ('pending', 'approved', 'cancelled');
 
 CREATE TABLE reservations (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  user_id uuid,
+  user_id uuid NOT NULL,
   device_quantities jsonb, -- format: { [ device_kind_id: string ]: number }
-  pickup_time_start timestamp with time zone,
-  pickup_time_end timestamp with time zone,
-  return_time_start timestamp with time zone,
-  return_time_end timestamp with time zone,
-  lab_id uuid,
-  status reservation_status,
-  created_at timestamp with time zone,
-  updated_at timestamp with time zone
+  pickup_time_start timestamp with time zone NOT NULL,
+  pickup_time_end timestamp with time zone NOT NULL,
+  return_time_start timestamp with time zone NOT NULL,
+  return_time_end timestamp with time zone NOT NULL,
+  lab_id uuid NOT NULL,
+  status reservation_status NOT NULL,
+  created_at timestamp with time zone NOT NULL,
+  updated_at timestamp with time zone NOT NULL
 );
 
 CREATE TABLE role_histories (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   grantee_id uuid,
   granter_id uuid,
   permissions jsonb,
-  effective_start timestamp with time zone,
-  effective_end timestamp with time zone
+  effective_start timestamp with time zone NOT NULL,
+  effective_end timestamp with time zone NOT NULL,
+  PRIMARY KEY(grantee_id, granter_id)
 );
 
 CREATE TABLE labs (
@@ -53,20 +53,20 @@ CREATE TABLE labs (
   faculty text CHECK ((char_length(name) <= 256)),
   room text CHECK ((char_length(name) <= 256)),
   branch text CHECK ((char_length(name) <= 256)),
-  timetable jsonb CHECK,
+  timetable jsonb NOT NULL,
   admin_id uuid
 );
 
 CREATE TABLE receipts (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  borrower_id uuid,
+  borrower_id uuid NOT NULL,
   checker_id uuid,
   quantity integer NOT NULL,
   borrowed_at time with time zone NOT NULL,
   expected_returned_at time with time zone NOT NULL,
   returned_at time with time zone,
-  device_id uuid,
-  lab_id uuid
+  device_id uuid NOT NULL,
+  lab_id uuid NOT NULL
 );
 
 CREATE TYPE device_quality AS ENUM ('healthy', 'needs fixing', 'broken', 'lost');
@@ -79,34 +79,34 @@ CREATE table devices (
   quantity integer DEFAULT 0,
   unit text CHECK ((char_length(name) <= 32)),
   lab_id uuid,
-  available_quantity integer, -- computed
-  quality device_quality,
-  borrowable_status device_borrowable_status,
-  meta jsonb,
+  available_quantity integer NOT NULL, -- computed
+  quality device_quality NOT NULL,
+  borrowable_status device_borrowable_status NOT NULL,
+  meta jsonb NOT NULL,
   borrower_id uuid
 );
 
 CREATE TABLE inventory_assessments (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   finished_at time with time zone,
-  lab_id uuid,
+  lab_id uuid NOT NULL,
   accountant_id uuid,
-  devices jsonb
+  devices jsonb NOT NULL
 );
 
 CREATE TABLE shipments (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   start_at time with time zone,
   arrive_at time with time zone,
-  devices jsonb,
+  devices jsonb NOT NULL,
   start_lab_id uuid NOT NULL,
   arrive_lab_id uuid NOT NULL
 );
 
 CREATE TABLE expiration_extension_requests (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   user_id uuid,
   receipt_id uuid,
-  status request_status,
-  return_at time with time zone
+  status request_status NOT NULL,
+  return_at time with time zone NOT NULL,
+  PRIMARY KEY(user_id, receipt_id)
 );
