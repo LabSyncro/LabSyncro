b35b6d77902a9e01df0ac0ad2305532f46c0bc13
Huy-DNA <huydo862003@gmail.com>
2024-10-03 10:26:48 +0700

    fix: lint issues

--- app.vue
+++ app.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
-const message = apiRef({
-  route: '/api/auth/login',
-  method: 'post',
-  defaultValue: (await useFetch('/api/auth/login', {
+  const message = apiRef({
+    route: '/api/auth/login',
     method: 'post',
-    body: {
-      email: 'phu.nguyen2310@hcmut.edu.vn',
-      password: '123456789'
-    }
-  })).data
-});
-
+    defaultValue: (await useFetch('/api/auth/login', {
+      method: 'post',
+      body: {
+        email: 'phu.nguyen2310@hcmut.edu.vn',
+        password: '123456789'
+      }
+    })).data
+  });
 </script>
 
 <template>
