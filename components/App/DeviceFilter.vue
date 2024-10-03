128e7eca6efdf7dcb3513127e67108ba0d72f6fd
Huy-DNA <huydo862003@gmail.com>
2024-10-03 19:35:07 +0700

    fix: styling of chevron

--- components/App/DeviceFilter.vue
+++ components/App/DeviceFilter.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
   import { facultyService, Faculty } from '@/services';
-
-  const currentCategory = ref<null | Faculty>(null);
+  const ALL_FACULTIES = facultyService.getAllFaculties;
+  const currentFaculty = ref<null | Faculty>(null);
 </script>
 
 <template>
   <UDropdown>
     <div class="bg-white border-gray-lighter border-2 px-2 pr-7 relative">
       <button class="h-[100%] m-0 p-0">
-        {{ currentCategory?.name || 'Tất cả các khoa' }}
+        {{ currentFaculty?.name || 'Tất cả các khoa' }}
       </button>
       <UIcon
         aria-hidden
-        class="absolute right-1 top-2.5 text-gray-light text-xl"
+        class="absolute right-1 top-2.5 text-gray-light text-lg"
         name="i-heroicons-chevron-down"
       />
     </div>
