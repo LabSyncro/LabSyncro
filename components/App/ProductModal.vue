f72ec33730b36564d599aceeed942d8cbad60d47
Huy-DNA <huydo862003@gmail.com>
2024-10-13 11:54:15 +0700

    feat: add shadow to product modal

--- components/App/ProductModal.vue
+++ components/App/ProductModal.vue
@@ -5,6 +5,8 @@
 </script>
 
 <template>
-  <div v-if="props.active" class="fixed animate-in">
+  <div role="menu" v-if="props.active" class="fixed top-[180px] text-black left-0 w-[100vw]">
+    <div class="p-5 shadow-[0_8px_8px_rgba(0,0,0,0.1)]">
+    </div>
   </div> 
 </template>
