6a8e0c21443a646acf7f72d8cae100f660725cf4
Huy-DNA <huydo862003@gmail.com>
2024-10-13 11:06:20 +0700

    feat: add QR button

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -13,10 +13,10 @@
       <div class="flex items-stretch justify-between gap-4 grow-[3]">
         <AppDeviceFilter class="w-[180px] h-11" /> 
         <AppSearchBox class="flex-1" />
+        <AppQrButton />
       </div>
       <div class="flex my-auto grow-[2] gap-3 justify-end">
         <AppAvatar />
-        <AppShoppingButton />
       </div>
     </div>
     <nav>
---
+++ components/App/QrButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="relative bg-tertiary-dark text-white rounded-md w-11">
+    <Icon
+      aria-hidden
+      class="absolute left-3 top-[12px] text-xl"
+      name="i-heroicons-qr-code"
+    />
+    &nbsp;
+  </button>
+</template>
