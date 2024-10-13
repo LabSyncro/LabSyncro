2b9ef25be5ad71b7df2101b3232eafb010502a28
Huy-DNA <huydo862003@gmail.com>
2024-10-13 11:07:07 +0700

    fix: height of QR button

--- components/App/QrButton.vue
+++ components/App/QrButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="relative bg-tertiary-dark text-white rounded-md w-11">
+  <button class="relative bg-tertiary-dark text-white rounded-md w-11 h-11">
     <Icon
       aria-hidden
       class="absolute left-3 top-[12px] text-xl"
