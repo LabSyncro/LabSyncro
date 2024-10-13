9d751851c0153d2ed7ae2199a9e4257d920b73f2
Huy-DNA <huydo862003@gmail.com>
2024-10-13 12:10:34 +0700

    feat: add slide-in animation for product modal

--- components/App/ProductModal.vue
+++ components/App/ProductModal.vue
@@ -5,8 +5,28 @@
 </script>
 
 <template>
-  <div role="menu" v-if="props.active" class="fixed top-[180px] text-black left-0 w-[100vw]">
+  <div role="menu" v-if="props.active" class="fixed top-[180px] text-black left-0 w-[100vw]" id="menu">
     <div class="p-5 shadow-[0_8px_8px_rgba(0,0,0,0.1)]">
     </div>
   </div> 
 </template>
+
+<style scoped>
+  #menu {
+    animation: menu-slide 0.3s linear;
+  }
+  @keyframes menu-slide {
+    0% {
+      opacity: 0;
+      transform: translateY(-10px);
+    }
+    30% {
+      opacity: 0.6;
+      transform: translateY(-2px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0px);
+    }
+  }
+</style>
