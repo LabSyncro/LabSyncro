fd81ffc7b7474328edb44de8486626d0ee12d7cd
Huy-DNA <huydo862003@gmail.com>
2024-10-13 11:03:51 +0700

    fix: searchbox padding

--- components/App/SearchBox.vue
+++ components/App/SearchBox.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="relative">
     <input
-      class="bg-white text-primary-light placeholder:text-primary-light border-2 h-11 w-[100%] pl-10 rounded-md text-md placeholder:text-normal"
+      class="bg-white text-primary-light placeholder:text-primary-light border-2 h-11 w-[100%] pl-10 pr-3 rounded-md text-md placeholder:text-normal"
       type="search"
       placeholder="Nhập tên thiết bị"
     >
