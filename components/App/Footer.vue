8345387b66bbe56da4bf2e11f3e1da5a707d7bda
Huy-DNA <huydo862003@gmail.com>
2024-10-22 15:52:06 +0700

    feat: add decription in footer

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -1,8 +1,9 @@
 <template>
-  <footer class="bg-primary-darker text-white py-16 px-8">
+  <footer class="bg-primary-darker text-white py-16 px-8 text-sm">
     <div class="flex gap-44 justify-between">
       <div class="flex-1">
-        <AppLogo class="w-[150px]"/>
+        <AppLogo class="w-[150px] mb-5"/>
+        LabSyncro là hệ thống Quản lý mượn trả trang thiết bị phòng thí nghiệm được hiện thực bởi nhóm sinh viên trong đề tài Luận văn.
       </div>
       <div class="flex-1"/>
     </div>
