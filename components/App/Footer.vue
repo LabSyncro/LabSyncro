90b10e855ae2e06643b6e78d1019b3b3e4f0bdf6
Huy-DNA <huydo862003@gmail.com>
2024-10-22 15:55:20 +0700

    fix: add links to other pages in footer

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -5,7 +5,11 @@
         <AppLogo class="w-[150px] mb-5"/>
         LabSyncro là hệ thống Quản lý mượn trả trang thiết bị phòng thí nghiệm được hiện thực bởi nhóm sinh viên trong đề tài Luận văn.
       </div>
-      <div class="flex-1"/>
+      <div class="flex-1 flex flex-col font-bold gap-2">
+        <a>Thông tin liên hệ và hỗ trợ</a>
+        <a>Văn bản Quy định</a>
+        <a>Đăng ký trực phòng thí nghiệm</a>
+      </div>
     </div>
   </footer>
 </template>
