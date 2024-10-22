bc05b074ba12984128b895cdf627082792b8cfd3
Huy-DNA <huydo862003@gmail.com>
2024-10-22 19:45:37 +0700

    fix: default bg to slate

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-[100vh] bg-gray-lighter text-gray-darker">
+  <div class="min-h-[100vh] bg-slate-light text-gray-darker">
     <AppHeader />
     <main class="min-h-[80vh]">
       <slot />
