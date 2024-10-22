734ea1c2318a6aa9f1ceacc191ca1f502673d277
Huy-DNA <huydo862003@gmail.com>
2024-10-22 15:39:14 +0700

    feat: allow closing dropdown when pressing escape

--- components/App/DeviceFilter.vue
+++ components/App/DeviceFilter.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { facultyService, Faculty } from '@/services';
 const allFaculties = await facultyService.getAllFaculties();
-const { isActive: isDropdownActive } = useClick('dropdown');
+const isDropdownActive = ref(false);
 const dropdownItems = [...allFaculties, { name: 'Tất cả các khoa', id: null }];
 const currentFacultyId = ref<null | string>(null);
 const currentFacultyName = computed(() => dropdownItems.find(({ id }) => id === currentFacultyId.value)?.name || null);
@@ -9,12 +9,20 @@ const currentFacultyName = computed(() => dropdownItems.find(({ id }) => id === 
 function setFaculty (id: string) {
   currentFacultyId.value = id;
 }
+
+function toggleDropdown () {
+  isDropdownActive.value = !isDropdownActive.value;
+}
+
+function closeDropdown () {
+  isDropdownActive.value = false;
+}
 </script>
 
 <template>
   <div class="relative pl-4 py-3 pr-8 rounded-md bg-white text-primary-light text-normal">
-    <div ref="dropdown" class="cursor-pointer">
-      <div class="h-[100%] m-0 p-0" role="dropdown">
+    <div class="cursor-pointer">
+      <div class="h-[100%] m-0 p-0" role="dropdown" @click="toggleDropdown">
         <p class="line-clamp-1 text-ellipsis">{{ currentFacultyName }}</p>
       </div>
       <Icon
@@ -25,9 +33,21 @@ function setFaculty (id: string) {
     </div>
     <div
       :class="`${ !isDropdownActive ? 'opacity-0 z-[-1]' : 'opacity-90 z-[60]' } animate-in animate-out fixed top-0 py-16 left-0 w-[100vw] h-[100vh] bg-black text-white`"
+      tabindex="1"
       role="menu"
+      @mouseenter="(e) => e.target.focus()"
+      @keydown.esc="closeDropdown"
     >
       <p class='text-center mb-5 text-lg'>Chọn Khoa để tìm thiết bị</p>
+      <button
+        class="absolute right-[16px] top-[16px] text-white text-md bg-secondary-lighter rounded-full w-8 h-8 m-0 p-0 flex items-center justify-center"
+        @click="closeDropdown"
+      >
+        <Icon
+          aria-hidden
+          name="i-heroicons-x-mark"
+        />
+      </button>
       <div class='flex justify-start gap-10 px-5'>
         <div
           v-for="(faculty, index) in allFaculties"
