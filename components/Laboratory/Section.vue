b9721094b2d527674b6c8c9c7f84aeadb3ac9312
Huy-DNA <huydo862003@gmail.com>
2024-10-23 16:08:57 +0700

    feat: basic laboratory menu

--- components/Laboratory/Section.vue
+++ components/Laboratory/Section.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { facultyService, Faculty } from '@/services';
+const allFaculties = await facultyService.getAllFaculties();
 </script>
 
 <template>
   <section class="bg-gray-200 py-6">
     <div class="flex">
-      <div class="bg-white py-5 pl-8 pr-12">
+      <div class="relative bg-white py-5 pl-12 pr-8 lg:pl-24 lg:pr-16 flex flex-col shadow-[0_0_24px_rgba(0,0,0,0.2)]">
+        <a
+          v-for="(faculty, index) in allFaculties"
+          :key="index"
+          class="text-sm my-2"
+        >
+          {{ faculty.name }}
+        </a>
       </div>
       <div class="bg-white flex-1 my-5 px-5 py-5">
       </div>
