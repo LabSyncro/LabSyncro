c3a3e80d0b0f8c1ddcac5d32855880ed10957e88
Huy-DNA <huydo862003@gmail.com>
2024-10-23 15:56:38 +0700

    chore: add more mocked faculties

--- components/Laboratory/Section.vue
+++ components/Laboratory/Section.vue
@@ -2,6 +2,12 @@
 </script>
 
 <template>
-  <section class="bg-gray-200 py-6 px-24">
+  <section class="bg-gray-200 py-6">
+    <div class="flex">
+      <div class="bg-white py-5 pl-8 pr-12">
+      </div>
+      <div class="bg-white flex-1 my-5 px-5 py-5">
+      </div>
+    </div>
   </section>
 </template>
--- services/faculty.ts
+++ services/faculty.ts
@@ -8,7 +8,17 @@ export const facultyService = {
   async getAllFaculties (): Promise<Faculty[]> {
     return [
       { name: 'Khoa Khoa Học và Kĩ thuật Máy tính', id: '0' },
-      { name: 'Khoa Điện tử - Viễn thông', id: '1' },
+      { name: 'Trung tâm Bảo dưỡng Công nghiệp', id: '1' },
+      { name: 'Khoa Cơ khí', id: '2' },
+      { name: 'Khoa Kỹ thuật Địa chất và Dầu khí', id: '3' },
+      { name: 'Khoa Điện - Điện tử', id: '4' },
+      { name: 'Khoa Kỹ thuật Giao thông', id: '5' },
+      { name: 'Khoa Kỹ thuật Hoá học', id: '6' },
+      { name: 'Khoa Môi trường và Tài nguyên', id: '7' },
+      { name: 'Khoa Quản lý Công nghiệp', id: '8' },
+      { name: 'Khoa Khoa học Ứng dụng', id: '9' },
+      { name: 'Khoa Công nghệ Vật liệu', id: '10' },
+      { name: 'Khoa Kỹ thuật Xây dựng', id: '11' },
     ];
   },
 };
