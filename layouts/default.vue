e73c28bdc2ea66ee9d4a02b3e60703064af940ea
Huy-DNA <huydo862003@gmail.com>
2024-11-03 18:49:15 +0700

    fix: change bg to gray-100

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-[100vh] bg-slate-light text-gray-darker">
+  <div class="min-h-[100vh] bg-gray-100 text-gray-darker">
     <AppHeader />
     <main class="min-h-[80vh]">
       <slot />
