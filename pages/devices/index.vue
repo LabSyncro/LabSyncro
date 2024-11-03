19972b9849f23f22523dba818e7dda66f9a92349
Huy-DNA <huydo862003@gmail.com>
2024-11-03 15:00:45 +0700

    fix: breadcrumb for devices page

--- pages/devices/index.vue
+++ pages/devices/index.vue
@@ -2,6 +2,26 @@
 </script>
 
 <template>
-  <div>
+  <div class="mx-16 my-10">
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <NuxtLink href="/" class="flex justify-center items-center text-lg">
+            <Icon
+              aria-hidden
+              name="i-heroicons-home"
+            />
+          </NuxtLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>
+          <p class="font-semibold">/</p>
+        </BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <p class="text-normal font-bold underline text-black">Cảm biến</p>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
   </div>
+  <main class="mx-16 my-10">
+  </main>
 </template>
