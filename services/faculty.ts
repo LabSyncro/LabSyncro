143be265e254658f0db8c43acd0791f415d0a9a3
Huy-DNA <huydo862003@gmail.com>
2024-11-03 17:10:12 +0700

    feat: integrate faculty api

--- services/faculty.ts
+++ services/faculty.ts
@@ -1,24 +1,12 @@
+import { sortBy } from 'lodash-es';
+
 export interface Faculty {
   name: string;
-  id: string;
+  id: number;
 };
 
 export const facultyService = {
-  // TODO: Replace with real implementation
-  async getAllFaculties (): Promise<Faculty[]> {
-    return [
-      { name: 'Khoa Khoa Học và Kĩ thuật Máy tính', id: '0' },
-      { name: 'Trung tâm Bảo dưỡng Công nghiệp', id: '1' },
-      { name: 'Khoa Cơ khí', id: '2' },
-      { name: 'Khoa Kỹ thuật Địa chất và Dầu khí', id: '3' },
-      { name: 'Khoa Điện - Điện tử', id: '4' },
-      { name: 'Khoa Kỹ thuật Giao thông', id: '5' },
-      { name: 'Khoa Kỹ thuật Hoá học', id: '6' },
-      { name: 'Khoa Môi trường và Tài nguyên', id: '7' },
-      { name: 'Khoa Quản lý Công nghiệp', id: '8' },
-      { name: 'Khoa Khoa học Ứng dụng', id: '9' },
-      { name: 'Khoa Công nghệ Vật liệu', id: '10' },
-      { name: 'Khoa Kỹ thuật Xây dựng', id: '11' },
-    ];
+  async getAllFaculties(): Promise<Faculty[]> {
+    return sortBy((await $fetch('/api/faculty')).faculties, ({ name }) => name);
   },
 };
