e4e474162417728ba050c3db8a0f45d2641203c1
Huy-DNA <huydo862003@gmail.com>
2024-11-03 10:39:17 +0700

    fix: remove unnecessary parts of DeviceBorrowSection

--- components/DeviceBorrow/Section.vue
+++ components/DeviceBorrow/Section.vue
@@ -3,10 +3,6 @@
 
 <template>
   <section class="mt-2 bg-white py-6 px-10">
-    <h2 class="text-tertiary-darker text-center text-lg">
-      Mượn thiết bị theo môn học
-    </h2>
-    <DeviceBorrowSearchBox />
     <DeviceBorrowList />
   </section>
 </template>
