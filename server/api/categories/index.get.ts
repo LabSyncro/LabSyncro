7999e6108fb89baa7f7f8aed9b5cd5024067c304
Huy-DNA <huydo862003@gmail.com>
2024-11-03 12:03:30 +0700

    fix: include more info in categories api

--- server/api/categories/index.get.ts
+++ server/api/categories/index.get.ts
@@ -4,13 +4,16 @@ import * as db from 'zapatos/db';
 import { dbPool } from '~/server/db';
 
 const CategoryOutputDto = Type.Object({
-  categories: Type.Array(Type.String()),
+  categories: Type.Array(Type.Object({
+    id: Type.Number(),
+    name: Type.String(),
+  })),
 });
 
 type CategoryOutputDto = Static<typeof CategoryOutputDto>;
 
 export default defineEventHandler<Promise<CategoryOutputDto>>(async () => {
-  const categories = (await db.select('categories', {}).run(dbPool)).map(({ name }) => name);
+  const categories = (await db.select('categories', {}).run(dbPool)).map(({ name, id }) => ({ id, name }));
   return {
     categories,
   };
