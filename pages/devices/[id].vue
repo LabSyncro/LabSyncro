876df501b4408d3de0bf03262fc2b83f62c75f86
Huy-DNA <huydo862003@gmail.com>
2024-11-03 15:02:48 +0700

    feat: breadcrumb for device detail page

--- pages/devices/[id].vue
+++ pages/devices/[id].vue
@@ -3,5 +3,33 @@
 
 <template>
   <div>
+    <div class="mx-16 my-10">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <NuxtLink href="/" class="flex justify-center items-center text-lg">
+              <Icon
+                aria-hidden
+                name="i-heroicons-home"
+              />
+            </NuxtLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <p class="font-semibold">/</p>
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <NuxtLink class="text-normal text-black" href="/devices">Cảm biến</NuxtLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <p class="font-semibold">/</p>
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <NuxtLink class="text-normal font-bold underline text-black"></NuxtLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+    <main class="mx-16 my-10">
+    </main>
   </div>
 </template>
