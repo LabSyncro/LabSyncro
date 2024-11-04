4d3f8ef147091d5afecd30d712dc9480c54dfd1f
Huy-DNA <huydo862003@gmail.com>
2024-11-04 11:49:35 +0700

    fix: renaming component

--- components/DeviceBorrow/Section.vue
+++ components/DeviceBorrow/Section.vue
@@ -7,7 +7,7 @@ const categories = await categoryService.getCategories();
 <template>
   <section class="mt-2 bg-white py-6 px-10">
     <div v-for="(category, index) in categories" :key="index" class="mb-10">
-      <DeviceBorrowList :category="category" />
+      <DeviceList :category="category" />
     </div>
   </section>
 </template>
