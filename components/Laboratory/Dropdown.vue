ed2c4cf313acf0f2db6ed7f92ca24fb6518925e4
Huy-DNA <huydo862003@gmail.com>
2024-11-06 20:09:55 +0700

    feat: basic dropdown

--- components/Laboratory/Dropdown.vue
+++ components/Laboratory/Dropdown.vue
@@ -17,12 +17,14 @@ function toggleDropdown() {
 </script>
 
 <template>
-  <div>
+  <div class="relative">
     <button class="relative text-left text-normal p-2.5 py-3 border-[1px] w-[100%] shadow-sm" @click="toggleDropdown">
       <p class="line-clamp-1">{{ props.lab.name }}</p>
       <Icon
         aria-hidden class="absolute top-4 right-2"
         :name="`${isDropdownActive ? 'i-heroicons-chevron-up' : 'i-heroicons-chevron-down'}`" />
     </button>
+    <div v-if="isDropdownActive" class="absolute p-2.5 bg-white z-50 w-[100%]">
+    </div>
   </div>
 </template>
