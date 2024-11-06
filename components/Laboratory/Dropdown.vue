663135b75773fd7c19d0e83d2ccafcbdc6aef627
Huy-DNA <huydo862003@gmail.com>
2024-11-06 20:21:16 +0700

    feat: basic timetable in laboratory dropdown

--- components/Laboratory/Dropdown.vue
+++ components/Laboratory/Dropdown.vue
@@ -24,7 +24,15 @@ function toggleDropdown() {
         aria-hidden class="absolute top-4 right-2"
         :name="`${isDropdownActive ? 'i-heroicons-chevron-up' : 'i-heroicons-chevron-down'}`" />
     </button>
-    <div v-if="isDropdownActive" class="absolute p-2.5 bg-white z-50 w-[100%]">
+    <div v-if="isDropdownActive" class="absolute px-2.5 py-1 bg-white z-50 w-[100%]">
+      <p class="text-sm font-bold text-slate-dark my-2">GIỜ MỞ CỬA (Dự kiến)</p>
+      <div v-for="[day, times] in Object.entries(props.lab.timetable)" :key="day" class="flex gap-1 text-sm">
+        <p>{{ day }}:</p>
+        <div>
+          <p v-for="time in times" :key="time">{{ time }}</p>
+        </div>
+      </div>
+      <p class="text-sm font-bold text-slate-dark my-2">LIÊN HỆ:</p>
     </div>
   </div>
 </template>
