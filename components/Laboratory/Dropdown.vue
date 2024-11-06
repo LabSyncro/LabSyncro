db1c3eb59bfc3a27e4215953fa81c1b78c6222b1
Huy-DNA <huydo862003@gmail.com>
2024-11-06 20:06:16 +0700

    feat: change chevron position based on whether the dropdown is active

--- components/Laboratory/Dropdown.vue
+++ components/Laboratory/Dropdown.vue
@@ -9,13 +9,16 @@ const props = defineProps<{
     room: string;
   };
 }>();
+const isDropdownActive = ref(false);
 </script>
 
 <template>
   <div>
     <button class="relative text-left text-normal p-2.5 py-3 border-[1px] w-[100%] shadow-sm">
       <p class="line-clamp-1">{{ props.lab.name }}</p>
-      <Icon aria-hidden class="absolute top-4 right-2" name="i-heroicons-chevron-down" />
+      <Icon
+        aria-hidden class="absolute top-4 right-2"
+        :name="`${isDropdownActive ? 'i-heroicons-chevron-up' : 'i-heroicons-chevron-down'}`" />
     </button>
   </div>
 </template>
