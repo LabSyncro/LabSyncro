b4d09cb07b8fcb40dd7a99909fa9f4581641f79c
Huy-DNA <huydo862003@gmail.com>
2024-11-06 20:32:19 +0700

    feat: more info about lab admin

--- server/api/laboratory/index.get.ts
+++ server/api/laboratory/index.get.ts
@@ -10,6 +10,8 @@ const LabOutputDto = Type.Object({
       branch: Type.String(),
       timetable: Type.Record(Type.String(), Type.Array(Type.String())),
       adminId: Type.String(),
+      adminName: Type.String(),
+      adminTel: Type.String(),
       name: Type.String(),
       room: Type.String(),
     })),
@@ -24,10 +26,12 @@ export default defineEventHandler<
 >(async (event) => {
   const { faculty } = getQuery(event);
   const labs = (await db.sql`
-    SELECT DISTINCT ${'labs'}.${'branch'}, ${'labs'}.${'room'}, ${'labs'}.${'timetable'}, ${'labs'}.${'admin_id'}, ${'labs'}.${'name'}
+    SELECT DISTINCT ${'labs'}.${'branch'}, ${'labs'}.${'room'}, ${'labs'}.${'timetable'}, ${'labs'}.${'admin_id'}, ${'labs'}.${'name'}, ${'users'}.${'name'} as admin_name, ${'users'}.${'email'} as admin_email, ${'users'}.${'tel'} as admin_tel
     FROM ${'labs'}
+      JOIN ${'users'}
+      ON ${'labs'}.${'admin_id'} = ${'users'}.${'id'}
     WHERE ${'labs'}.${'faculty'} = ${db.param(faculty)}
-  `.run(dbPool)).map(({ branch, room, timetable, admin_id, name }) => ({ branch, room, timetable: mapValues(timetable, (time) => [time]), adminId: admin_id, name }));
+  `.run(dbPool)).map(({ branch, room, timetable, admin_id, name, admin_name, admin_email, admin_tel }) => ({ branch, room, timetable: mapValues(timetable, (time) => [time]), adminId: admin_id, name, adminName: admin_name, adminEmail: admin_email, adminTel: admin_tel }));
   const _branches = groupBy(labs, (lab) => lab.branch);
   const branches = [];
   for (const key in _branches) {
