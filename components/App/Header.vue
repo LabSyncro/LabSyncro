121edb7811053f8293783df422982c74b2c55b5f
Huy-DNA <huydo862003@gmail.com>
2024-11-13 19:14:44 +0700

    fix: make the header look decent on small screens

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -3,20 +3,21 @@
 
 <template>
   <div>
-    <div
-      class="bg-primary-darker px-14 py-9 lg:px-32"
-      role="banner"
-    >
+    <div class="bg-primary-darker px-14 py-9 lg:px-32" role="banner">
+      <div class="flex md:hidden mb-10 justify-between items-center">
+        <AppLogo />
+        <AppAvatar />
+      </div>
       <div class="flex gap-16 justify-between">
-        <div class="grow-[1] flex items-center">
+        <div class="grow-[1] hidden md:flex items-center">
           <AppLogo />
         </div>
         <div class="flex items-stretch justify-between gap-4 grow-[3]">
-          <AppDeviceFilter class="w-[180px] h-11" /> 
+          <AppDeviceFilter class="h-11" />
           <AppSearchBox class="flex-1" />
           <AppQrButton />
         </div>
-        <div class="flex my-auto grow-[2] gap-3 justify-end">
+        <div class="my-auto hidden md:flex grow-[2] gap-3 justify-end">
           <AppAvatar />
         </div>
       </div>
