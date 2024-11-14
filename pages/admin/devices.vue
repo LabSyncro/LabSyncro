7cc752443f21508c2b9c0377a53bcd8e48cccf20
Huy-DNA <huydo862003@gmail.com>
2024-11-14 22:10:58 +0700

    feat: add table to admin device page

--- pages/admin/devices.vue
+++ pages/admin/devices.vue
@@ -1,4 +1,12 @@
+<script setup lang="ts">
+import { columns } from '~/components/app/DeviceTable/column';
+import type { AdminDeviceList } from '~/components/app/DeviceTable/schema';
+
+const data = ref<AdminDeviceList[]>([]);
+</script>
+
 <template>
   <div>
+    <DeviceTable :columns="columns" :data="data" />
   </div>
 </template>
