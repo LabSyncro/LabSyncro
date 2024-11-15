4448a247e756124c6d38acd148a7c187acf704eb
Huy-DNA <huydo862003@gmail.com>
2024-11-15 13:40:02 +0700

    fix: update compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ env.config();
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2024-11-15',
   devtools: { enabled: true },
   modules: [
     '@nuxt/eslint',
