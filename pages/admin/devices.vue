7465a24e40803d9e7d861f1c059916c5768bcae1
Huy-DNA <huydo862003@gmail.com>
2024-11-17 00:25:07 +0700

    fix: make the device table more responsive

--- pages/admin/devices.vue
+++ pages/admin/devices.vue
@@ -20,6 +20,8 @@ onMounted(async () => {
 watch(paginationState, async () => {
   data.value = (await deviceKindService.getDeviceKinds(paginationState.value.pageIndex * paginationState.value.pageSize, paginationState.value.pageSize)).deviceKinds;
 });
+
+const searchText = ref('');
 </script>
 
 <template>
@@ -63,18 +65,21 @@ watch(paginationState, async () => {
         <div class="flex justify-between items-stretch">
           <div class="relative items-center flex gap-4 m-auto md:m-0 md:mb-8 mb-8">
             <input type="search" placeholder="Nhập tên/mã thiết bị"
-              class="border-gray-300 border rounded-sm p-2 pl-10 min-w-[300px]">
+              class="border-gray-300 border rounded-sm p-2 pl-10 w-[250px] sm:w-[300px] md:w-[350px] lg:w-[400px]" v-model="searchText">
             <Icon aria-hidden class="absolute left-3 top-[12px] text-xl text-primary-dark"
               name="i-heroicons-magnifying-glass" />
-            <button class="relative bg-slate-200 border border-slate-400 text-slate-dark rounded-md w-11 h-11 lg:w-auto lg:h-auto">
+            <button class="relative bg-slate-200 border border-slate-400 text-slate-dark rounded-md w-11 h-11 lg:w-auto">
               <Icon aria-hidden class="absolute left-3 top-[12px] text-xl" name="i-heroicons-qr-code" />
               <p class="hidden lg:block pl-10 pr-3">Quét QR thiết bị</p>
             </button>
+            <button class="relative md:hidden bg-tertiary-darker items-center text-white px-3 rounded-md w-11 h-11">
+              <Icon aria-hidden class="absolute left-3 top-[12px] text-xl" name="i-heroicons-plus" />
+            </button>
           </div>
           <div>
-            <button class="relative bg-tertiary-darker items-center text-white h-11 px-3 pl-10 rounded-md">
+            <button class="relative hidden md:block bg-tertiary-darker items-center text-white px-3 rounded-md w-11 h-11 md:w-auto">
               <Icon aria-hidden class="absolute left-3 top-[12px] text-xl" name="i-heroicons-plus" />
-              <span>Bổ sung thiết bị</span>
+              <span class="hidden md:block pl-8 pr-3">Bổ sung thiết bị</span>
             </button>
           </div>
         </div>
