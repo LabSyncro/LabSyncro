65872a47edd0ae30abfb66de45d3adee9d155fb8
Huy-DNA <huydo862003@gmail.com>
2024-11-20 16:50:00 +0700

    perf: implement request caching

--- services/laboratory.ts
+++ services/laboratory.ts
@@ -2,6 +2,9 @@ import { sortBy } from 'lodash-es';
 
 export const laboratoryService = {
   async getAllLabsByFaculty (faculty: string): Promise<Record<string, unknown>[]> {
-    return sortBy((await $fetch('/api/laboratory', { query: { faculty } })).branches, ({ name }) => name);
+    return sortBy((await $fetch('/api/laboratory', {
+      query: { faculty },
+      ttl: 3600,
+    })).branches, ({ name }) => name);
   }
 };
