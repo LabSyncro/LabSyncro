e0f1062c6fb52e4c9a9a13e5f5ace98fca590cc9
Huy-DNA <huydo862003@gmail.com>
2024-11-20 17:00:54 +0700

    feat: basic dropdown for homepage search box

--- components/app/DeviceSearchBox.vue
+++ components/app/DeviceSearchBox.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="relative">
-    <input
-      class="bg-white text-primary-light placeholder:text-primary-light border-2 h-11 w-[100%] pl-10 pr-3 rounded-md text-md placeholder:text-normal"
-      type="search"
-      placeholder="Tên loại thiết bị"
-    >
-    <Icon
-      aria-hidden
-      class="absolute left-3 top-[12px] text-xl text-primary-dark"
-      name="i-heroicons-magnifying-glass"
-    />
+    <div class="relative">
+      <input
+        class="bg-white text-primary-light placeholder:text-primary-light border-2 h-11 w-[100%] pl-10 pr-3 rounded-md text-md placeholder:text-normal"
+        type="search"
+        placeholder="Tên loại thiết bị"
+      >
+      <Icon
+        aria-hidden
+        class="absolute left-3 top-[12px] text-xl text-primary-dark"
+        name="i-heroicons-magnifying-glass"
+      />
+    </div>
+
+    <div class="absolute bg-white p-1 mt-1 w-[100%] flex flex-col gap-1">
+      <NuxtLink class="flex gap-1 justify-between items-center py-1 px-2 rounded-md bg-primary-darker text-white" href="/devices">
+        <p> Xem toàn bộ thiết bị </p>
+        <Icon
+          aria-hidden
+          name="i-heroicons-chevron-double-right text-lg"
+        />
+      </NuxtLink>
+    </div>
   </div>
 </template>
