1799dca389b460d7478e979dfbfb2ff7ae6aefc7
Huy-DNA <huydo862003@gmail.com>
2024-11-20 19:29:00 +0700

    feat: allow using arrow up and down to navigate the search items

--- components/app/DeviceSearchBox.vue
+++ components/app/DeviceSearchBox.vue
@@ -10,6 +10,7 @@ function openDropdown () {
 }
 
 function closeDropdown () {
+  focusedSearchItemIndex.value = null;
   isDropdownActive.value = false;
 }
 
@@ -17,11 +18,34 @@ async function handleClickOutsideOfSearchBox () {
   setTimeout(closeDropdown, 300);
 }
 
+const focusedSearchItemIndex = ref(null);
+
+const numberOfSearchItemsShown = 4;
+
 const searchItems = ref<{ name: string; image: string }[]>([]);
 watch(searchText, async () => {
-  const data = await deviceKindService.getDeviceKinds(0, 4, { searchText: searchText.value || undefined, searchFields: ['device_kind_id', 'device_name'] });
+  focusedSearchItemIndex.value = null;
+  const data = await deviceKindService.getDeviceKinds(0, numberOfSearchItemsShown, { searchText: searchText.value || undefined, searchFields: ['device_kind_id', 'device_name'] });
   searchItems.value = data.deviceKinds.map(({ name, mainImage, id }) => ({ id, name, image: mainImage }));
 });
+
+function focusNextSearchItem () {
+  if (focusedSearchItemIndex.value === null) focusedSearchItemIndex.value = -1;
+  focusedSearchItemIndex.value = (focusedSearchItemIndex.value + 1) % numberOfSearchItemsShown;
+}
+
+function focusPrevSearchItem () {
+  if (focusedSearchItemIndex.value === null) focusedSearchItemIndex.value = 0;
+  focusedSearchItemIndex.value = (focusedSearchItemIndex.value - 1 + numberOfSearchItemsShown) % numberOfSearchItemsShown;
+}
+
+function goToSearchItem () {
+  if (focusedSearchItemIndex.value === null) return;
+  const router = useRouter();
+  router.push(`/devices/${searchItems.value[focusedSearchItemIndex.value].id}`)
+  searchText.value = '';
+  isDropdownActive.value = false;
+}
 </script>
 
 <template>
@@ -29,13 +53,13 @@ watch(searchText, async () => {
     <div class="relative">
       <input v-model="searchText"
         class="bg-white text-primary-light placeholder:text-primary-light border-2 h-11 w-[100%] pl-10 pr-3 rounded-md text-md placeholder:text-normal"
-        type="search" placeholder="Tên loại thiết bị" @focus="openDropdown" @blur="handleClickOutsideOfSearchBox">
+        type="search" placeholder="Tên loại thiết bị" @click="openDropdown" @blur="handleClickOutsideOfSearchBox" @keydown.down="focusNextSearchItem" @keydown.up="focusPrevSearchItem" @keydown.enter="goToSearchItem">
       <Icon aria-hidden class="absolute left-3 top-[12px] text-xl text-primary-dark"
         name="i-heroicons-magnifying-glass" />
     </div>
 
     <div :class="`${isDropdownActive ? 'flex' : 'hidden'} flex-col gap-1 absolute bg-white p-1 mt-1 w-[120%] z-50`">
-      <NuxtLink v-for="item in searchItems" :key="item" class="px-2 text-normal p-1 flex gap-2 hover:bg-gray-100" :href="`/devices/${item.id}`">
+      <NuxtLink v-for="(item, index) in searchItems" :key="item" :class="`px-2 text-normal p-1 flex gap-2 hover:bg-gray-100 ${focusedSearchItemIndex === index ? 'bg-secondary-light' : ''}`" :href="`/devices/${item.id}`">
         <img :src="item.image" class="h-6">
         <HighlightText class="line-clamp-1" :text="item.name" :match-text="searchText || undefined" />
       </NuxtLink>
