98fcfc5fb6294070127f78af14f7a5c549be2975
Huy-DNA <huydo862003@gmail.com>
2024-11-22 20:21:12 +0700

    fix: widen column name + remove image + add device kind id to device table

--- components/app/DeviceTable/column.ts
+++ components/app/DeviceTable/column.ts
@@ -6,7 +6,7 @@ import RowAction from './RowAction.vue';
 
 import type { AdminDeviceList } from './schema';
 
-export function createColumns ({ sortField, sortOrder, rowSelection, onSelectRows }: { sortField: string | undefined; sortOrder: 'desc' | 'asc' | undefined; rowSelection: unknown[]; onSelectRows: (_: unknown[]) => void; }): ColumnDef<AdminDeviceList>[] {
+export function createColumns({ sortField, sortOrder, rowSelection, onSelectRows }: { sortField: string | undefined; sortOrder: 'desc' | 'asc' | undefined; rowSelection: unknown[]; onSelectRows: (_: unknown[]) => void; }): ColumnDef<AdminDeviceList>[] {
   return [
     {
       accessorKey: 'select',
@@ -19,34 +19,36 @@ export function createColumns ({ sortField, sortOrder, rowSelection, onSelectRow
           class: 'translate-y-0.5',
         }),
       cell: ({ row }) =>
-        h(Checkbox, {
-          checked: rowSelection.includes(row.original.id),
-          'onUpdate:checked': () => onSelectRows([row.original.id]),
-          ariaLabel: 'Select row',
-          class: 'translate-y-0.5',
-        }),
+        h('div', { class: 'flex items-center' },
+          [h(Checkbox, {
+            checked: rowSelection.includes(row.original.id),
+            'onUpdate:checked': () => onSelectRows([row.original.id]),
+            ariaLabel: 'Select row',
+            class: 'translate-y-0.5',
+          })],
+        ),
       enableSorting: false,
       enableHiding: false,
     },
     {
       accessorKey: 'name',
-      header: ({ column }) => h(ColumnHeader, { id: 'name', sortField, sortOrder, class: 'w-[250px] sm:w-[300px]', column, title: 'Tên loại thiết bị' }),
+      header: ({ column }) => h(ColumnHeader, { id: 'name', sortField, sortOrder, class: 'w-[300px] sm:w-[400px]', column, title: 'Tên loại thiết bị' }),
       cell: ({ row }) =>
         h(
           NuxtLink,
           {
-            class: 'justify-center items-center gap-3 inline-flex',
+            class: 'justify-start items-center gap-3 flex',
             href: `/devices/${row.original.id}`,
           },
           [
-            h('img', {
-              src: row.original.mainImage,
-              alt: row.original.name,
-              class: 'w-8 h-8 relative object-cover rounded-lg',
-            }),
             h(
               'p',
-              { class: 'line-clamp-2 text-slate-500 text-normal leading-6 font-normal leading-none' },
+              { class: 'p-1 px-2 bg-gray-100 border border-gray-300 rounded-md text-normal font-normal leading-none' },
+              row.original.id.toUpperCase(),
+            ),
+            h(
+              'p',
+              { class: 'line-clamp-2 text-slate-500 text-normal leading-6 font-normal' },
               row.getValue('name'),
             ),
           ],
@@ -69,7 +71,7 @@ export function createColumns ({ sortField, sortOrder, rowSelection, onSelectRow
     },
     {
       accessorKey: 'borrowableQuantity',
-      header: ({ column }) => h(ColumnHeader, { id: 'borrowable_quantity', sortField, sortOrder, column, title: 'Có thể mượn' }),
+      header: ({ column }) => h(ColumnHeader, { id: 'borrowable_quantity', sortField, sortOrder, column, title: 'Còn' }),
       cell: ({ row }) => h(
         'p',
         { class: 'text-slate-500 text-sm font-normal leading-tight text-right' },
