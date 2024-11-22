1317ad24b71b2cea92c6d0879395a2fad98c852f
Huy-DNA <huydo862003@gmail.com>
2024-11-22 09:32:30 +0700

    fix: wrong link in breadcrumb

--- pages/admin/borrows/form.vue
+++ pages/admin/borrows/form.vue
@@ -11,7 +11,7 @@
           <p class="font-semibold">/</p>
         </BreadcrumbSeparator>
         <BreadcrumbItem>
-          <NuxtLink class="text-normal font-bold underline text-black" href="/admin/devices">Mượn thiết bị</NuxtLink>
+          <NuxtLink class="text-normal font-bold underline text-black" href="/admin/borrows/form">Mượn thiết bị</NuxtLink>
         </BreadcrumbItem>
       </BreadcrumbList>
     </Breadcrumb>
