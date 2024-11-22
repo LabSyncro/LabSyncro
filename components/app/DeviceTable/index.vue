64f3a95a895a82d0853022ef83fd9518e48035c2
Huy-DNA <huydo862003@gmail.com>
2024-11-23 01:10:09 +0700

    fix: basic operations on DataTable in DeviceTable

--- components/app/DeviceTable/index.vue
+++ components/app/DeviceTable/index.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-async function fetchData () {
+import { deviceKindService } from '~/services';
+
+async function deleteData (ids: string[]) {
+  await deviceKindService.deleteByIds(ids);
 }
 
-async function deleteRows () {
+async function fetchData (offset: number, length: number, options: { desc?: boolean, sortField?: string, searchText?: string, searchFields?: string[] }): Promise<{ data: unknown[], totalPages: number }> {
+  const res = await deviceKindService.getDeviceKinds(offset, length, { searchText: options.searchText, searchFields: options.searchFields as any[], sortField: options.sortField as any, desc: options.desc });
+  return {
+    data: res.deviceKinds,
+    totalPages: res.totalPages,
+  };
 }
 
 </script>
