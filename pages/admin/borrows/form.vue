fbe1433be91ce6573140486846caeef76b109133
Huy-DNA <huydo862003@gmail.com>
2024-11-22 09:31:14 +0700

    feat: init borrow form

---
+++ pages/admin/borrows/form.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mx-6 sm:mx-16 my-10">
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <NuxtLink href="/" class="flex justify-center items-center text-lg">
+            <Icon aria-hidden name="i-heroicons-home" />
+          </NuxtLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>
+          <p class="font-semibold">/</p>
+        </BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <NuxtLink class="text-normal font-bold underline text-black" href="/admin/devices">Mượn thiết bị</NuxtLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  </div>
+</template>
