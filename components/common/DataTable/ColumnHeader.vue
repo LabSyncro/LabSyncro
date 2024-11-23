854bcc9780ba118349e5107e8339774dfe451af1
Huy-DNA <huydo862003@gmail.com>
2024-11-23 12:21:06 +0700

    fix: center align the sort icon

--- components/common/DataTable/ColumnHeader.vue
+++ components/common/DataTable/ColumnHeader.vue
@@ -39,12 +39,12 @@ function toggleSortOrder () {
   <div :class="cn('flex items-center space-x-2', $attrs.class ?? '')">
     <DropdownMenu>
       <DropdownMenuTrigger as-child>
-        <Button variant="ghost" size="sm" class="relative h-8 data-[state=open]:bg-accent text-md pr-8 text-black" @click="toggleSortOrder()">
+        <Button variant="ghost" size="sm" class="h-8 data-[state=open]:bg-accent text-md pr-8 text-black" @click="toggleSortOrder()">
           <span>{{ title }}</span>
-          <span v-if="column.columnDef.enableSorting">
-            <Icon v-if="sortField === id && sortOrder === 'asc'" aria-hidden class="text-xl absolute right-1" name="i-heroicons-arrow-long-down" />
-            <Icon v-else-if="sortField === id && sortOrder === 'desc'" aria-hidden class="text-xl absolute right-1" name="i-heroicons-arrow-long-up" />
-            <Icon v-else-if="sortField !== id" aria-hidden class="text-xl absolute right-1" name="i-heroicons-arrows-up-down" />
+          <span v-if="column.columnDef.enableSorting" class="flex items-center ml-2">
+            <Icon v-if="sortField === id && sortOrder === 'asc'" aria-hidden class="text-xl right-1" name="i-heroicons-arrow-long-down" />
+            <Icon v-else-if="sortField === id && sortOrder === 'desc'" aria-hidden class="text-xl right-1" name="i-heroicons-arrow-long-up" />
+            <Icon v-else-if="sortField !== id" aria-hidden class="text-xl right-1" name="i-heroicons-arrows-up-down" />
           </span>
         </Button>
       </DropdownMenuTrigger>
