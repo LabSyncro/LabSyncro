8c43a5005e648bb7439a6d9decf41fe3e2231f8f
Huy-DNA <huydo862003@gmail.com>
2024-11-24 13:11:35 +0700

    fix: use strip_vietnamese_accent db function to strip accent before matching strings

--- server/api/device_kinds/index.get.ts
+++ server/api/device_kinds/index.get.ts
@@ -58,8 +58,8 @@ export default defineEventHandler<
         ${categoryId !== undefined ? db.raw(`device_kinds.category_id = ${categoryId}`) : db.raw('TRUE')} AND
         ${'devices'}.${'deleted_at'} IS NULL
         ${searchText !== undefined ? db.raw(`AND (
-          (${searchFields?.includes('device_id') || false} AND devices.kind || '/' || devices.id ILIKE '%${searchText}%') OR
-          (${searchFields?.includes('device_name') || false} AND CAST(device_kinds.name AS TEXT) ILIKE '%${searchText}%')
+          (${searchFields?.includes('device_id') || false} AND strip_vietnamese_accents(devices.kind || '/' || devices.id) ILIKE strip_vietnamese_accents('%${searchText}%')) OR
+          (${searchFields?.includes('device_name') || false} AND strip_vietnamese_accents(CAST(device_kinds.name AS TEXT)) ILIKE strip_vietnamese_accents('%${searchText}%'))
         )`) : db.raw('')}
       GROUP BY ${'device_kinds'}.${'id'}
       ORDER BY ${sortField ? db.raw(`${sortField}`) : db.raw('device_kinds.id')} ${desc ? db.raw('DESC') : db.raw('ASC')}
@@ -77,8 +77,8 @@ export default defineEventHandler<
       ${'devices'}.${'deleted_at'} IS NULL AND
       ${'device_kinds'}.${'deleted_at'} IS NULL
       ${searchText !== undefined ? db.raw(`AND (
-        (${searchFields?.includes('device_id') || false} AND CAST(devices.id AS TEXT) ILIKE '%${searchText}%') OR
-        (${searchFields?.includes('device_name') || false} AND CAST(device_kinds.name AS TEXT) ILIKE '%${searchText}%')
+        (${searchFields?.includes('device_id') || false} AND strip_vietnamese_accents(CAST(devices.id AS TEXT)) ILIKE strip_vietnamese_accents('%${searchText}%')) OR
+        (${searchFields?.includes('device_name') || false} AND strip_vietnamese_accents(CAST(device_kinds.name AS TEXT)) ILIKE strip_vietnamese_accents('%${searchText}%'))
       )`) : db.raw('')}
     `).run(dbPool);
 
