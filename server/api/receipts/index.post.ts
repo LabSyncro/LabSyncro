20c09318280154f6de96121f5b2e2681993e7f55
Huy-DNA <huydo862003@gmail.com>
2024-11-24 16:36:47 +0700

    wip

---
+++ server/api/receipts/index.post.ts
@@ -0,0 +1,31 @@
+import * as db from 'zapatos/db';
+import { dbPool } from '~/server/db';
+import { Value } from '@sinclair/typebox/value';
+import { Type, type Static } from '@sinclair/typebox';
+import { BAD_REQUEST_CODE } from '~/constants';
+
+const BodyDto = Type.Object({
+  device_ids: Type.Array(Type.String()),
+  borrower_id: Type.String(),
+  borrow_date: Type.Date(),
+  borrow_lab_id: Type.String(),
+  expected_return_date: Type.Date(),
+  expected_return_lab_id: Type.String(),
+});
+
+type BodyDto = Static<typeof BodyDto>;
+
+export default defineEventHandler<
+  { body: BodyDto }
+>(async (event) => {
+  const body = Value.Convert(BodyDto, await readBody(event));
+
+  if (!Value.Check(BodyDto, body)) {
+    throw createError({
+      statusCode: BAD_REQUEST_CODE,
+      message: 'Bad request: Invalid body',
+    });
+  }
+
+  const { device_ids: deviceIds, borrower_id: borrowerId, borrow_date: borrowDate, borrow_lab_id: borrowLabId, expected_return_date: expectedReturnDate, expected_return_lab_id: expectedReturnLabId } = body;
+});
