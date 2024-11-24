4802cf25152727867c59923d6b132fba45e319c1
Huy-DNA <huydo862003@gmail.com>
2024-11-24 14:08:02 +0700

    fix: highlight regardless of accent in search text

--- components/common/HighlightText.vue
+++ components/common/HighlightText.vue
@@ -3,12 +3,30 @@ const props = defineProps<{
   matchText?: string;
   text: string;
 }>();
+
+function getRegexFromText (text: string): RegExp {
+  const ignoreAccentText = text
+    .replaceAll('(', '\\(')
+    .replaceAll('ă', '(?:[ăắằặẳ])')
+    .replaceAll('â', '(?:[âấầậẩẫ])')
+    .replaceAll('a', '(?:[aàáạảãăắằặẳâấầậẩẫ])')
+    .replaceAll('ơ', '(?:[ơớờởợỡ])')
+    .replaceAll('ô', '(?:[ôộồốổỗ])')
+    .replaceAll('o', '(?:[oòóọỏõơớờởợỡôộồốổỗ])')
+    .replaceAll('ê', '(?:[êếềệễể])')
+    .replaceAll('e', '(?:[eèéẻẽẹêếềệễể])')
+    .replaceAll('ư', '(?:[ưựữửừứ])')
+    .replaceAll('u', '(?:[uùúụũủưựữửừứ])')
+    .replaceAll('i', '(?:[iíìịỉĩ])')
+    .replaceAll('d', '(?:[dđ])');
+  return new RegExp(`(${ignoreAccentText})`, 'ig');
+}
 </script>
 
 <template>
   <p v-if="matchText">
     <span
-v-for="(fragment, index) in props.text.split(new RegExp(`(${matchText!.replaceAll('(', '\\(')})`, 'ig'))"
+      v-for="(fragment, index) in props.text.split(getRegexFromText(matchText!))"
       :key="index">
       <span :class="index % 2 ? 'text-tertiary-dark' : ''">{{ fragment }}</span>
     </span>
